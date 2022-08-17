56a684831a3e9d92534beab748b75c5d8db25b16
Ivan Vodopianov <vis3010@gmail.com>
2022-08-17 16:38:04 +0300

    the end task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector('ul#ingredients');
+
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = ingredients[i];
+
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+
+  elements.push(liEl)
+}
+
+ingredientsEl.append(...elements);
+
+console.log(ingredientsEl);
+
