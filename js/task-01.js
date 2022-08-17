e6f24b2970ccd71e80549ede190c595715ae9132
Ivan Vodopianov <vis3010@gmail.com>
2022-08-17 15:14:58 +0300

    the end task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,12 @@
-const categoriesEl = document.querySelector('ul#categories');  
+const categoriresEl = document.querySelector('ul#categories');
 const itemEl = document.querySelectorAll('li.item');
-console.log(itemEl);
 
+console.log(`Number of categories: ${itemEl.length}`);
+console.log('\n');
+
+const ul = document.querySelectorAll('#categories>li');
+ul.forEach(elem => {
+  console.log(`Category: ${elem.firstElementChild.textContent}`);
+  console.log(`Elements: ${elem.lastElementChild.children.length}`);
+  console.log('\n');
+});
