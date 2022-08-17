5540d09af69bf96f72ebec3a5803f3ef5c86221a
Ivan Vodopianov <vis3010@gmail.com>
2022-08-17 20:56:57 +0300

    corrected

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,4 @@
-const Ref = {
+const refs = {
   decrementBtn: document.querySelector('[data-action="decrement"]'),
   incrementBtn: document.querySelector(['data-action="increment"']),
   valueEl: document.querySelector('#value'),
