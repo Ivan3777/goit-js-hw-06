629dbcbf37da43c9f5e34d5261879f7af2c02ebe
Ivan Vodopianov <vis3010@gmail.com>
2022-08-17 18:47:12 +0300

    task-03 in process

---
+++ js/task-04.js
@@ -0,0 +1,7 @@
+const Ref = {
+  decrementBtn: document.querySelector('[data-action="decrement"]'),
+  incrementBtn: document.querySelector(['data-action="increment"']),
+  valueEl: document.querySelector('#value'),
+};
+
+
