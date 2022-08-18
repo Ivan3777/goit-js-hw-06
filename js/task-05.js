118b74f4b144bc745a0b7275974ea62582bd3983
Ivan Vodopianov <vis3010@gmail.com>
2022-08-18 17:33:37 +0300

    end task-05

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const formEl = document.querySelector('#name-input');
+const userName = document.querySelector('#name-output');
+
+formEl.addEventListener('input', () => 
+formEl.value !== ''
+? (userName.textContent = formEl.value) 
+: (userName.textContent = 'Anonymous'),);
+
+
+
