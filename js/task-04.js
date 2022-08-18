f2851fcb90c1f782d0d066ddced58fe96294b8aa
Ivan Vodopianov <vis3010@gmail.com>
2022-08-18 16:13:53 +0300

    end task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,22 @@
 const refs = {
   decrementBtn: document.querySelector('[data-action="decrement"]'),
-  incrementBtn: document.querySelector(['data-action="increment"']),
+  incrementBtn: document.querySelector('[data-action="increment"]'),
   valueEl: document.querySelector('#value'),
 };
+
+refs.decrementBtn.addEventListener('click', onDecrementBtnClick);
+refs.incrementBtn.addEventListener('click', onIncrementBtnClick);
+
+let counterValue = 0;
+
+function onDecrementBtnClick() {
+  counterValue -= 1;
+  refs.valueEl.textContent = counterValue;
+}
+
+function onIncrementBtnClick() {
+  counterValue += 1;
+  refs.valueEl.textContent = counterValue;
+}
 
 
