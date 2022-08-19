6aee7d147b31f2134e4ae2fa4ed71cf961a08d68
Ivan Vodopianov <vis3010@gmail.com>
2022-08-19 18:42:31 +0300

    the end task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButtonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color');
+
+changeButtonEl.addEventListener('click', event => {
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorEl.textContent = color;
+})
