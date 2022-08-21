0280dc3b6b8b6382935b7ccd4c777e21f712a65e
Ivan Vodopianov <vis3010@gmail.com>
2022-08-21 21:50:00 +0300

    corrected on the comment of the mentor

--- js/task-02.js
+++ js/task-02.js
@@ -8,21 +8,33 @@ const ingredients = [
 ];
 
 
+// ===== corrected on the comment of the mentor =====
+
 const ingredientsEl = document.querySelector('ul#ingredients');
 
-const elements = [];
-
-for (let i = 0; i < ingredients.length; i += 1) {
-  const ingredient = ingredients[i];
-
-  const liEl = document.createElement('li');
-  liEl.textContent = ingredient;
-  liEl.classList.add('item');
-
-  elements.push(liEl)
-}
+const elements = ingredients.map(ingredient => {
+  const element = document.createElement('li');
+  element.classList.add('item');
+  element.textContent = ingredient;
+  return element
+});
 
 ingredientsEl.append(...elements);
 
 console.log(ingredientsEl);
 
+
+
+
+
+//===== old method =====
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredient = ingredients[i];
+
+//   const liEl = document.createElement('li');
+//   liEl.textContent = ingredient;
+//   liEl.classList.add('item');
+
+//   elements.push(liEl)
+// }
